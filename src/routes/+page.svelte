d01ceb1d07fd78bb88fb88caca4b068a84433944
NullCode1337 <ishaqahmed.dev@gmail.com>
2024-11-19 00:02:09 +0600

    Add about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit"><span class="font-serif">svelte.dev/docs/kit</span></a> to read the documentation</p>
+<h1>Still WIP!</h1>
