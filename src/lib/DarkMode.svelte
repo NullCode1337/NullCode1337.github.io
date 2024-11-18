98e9dfb477a31724f17e20cbfcd98b2b153fcda2
NullCode1337 <ishaqahmed.dev@gmail.com>
2024-11-19 01:03:26 +0600

    Add experimental dark mode toggle

---
+++ src/lib/DarkMode.svelte
@@ -0,0 +1,18 @@
+<script>
+  let darkMode = false;
+  const toggle = () => {
+    darkMode = !darkMode;
+    window.document.body.classList.toggle("dark");
+  };
+</script>
+
+<button
+  class="bg-indigo-500 border-2 border-solid rounded-md pt-10 pb-10 pl-15 pr-15 text-amber-200"
+  on:click={toggle}
+>
+  {#if darkMode}
+    ☀️
+  {:else}
+    🌙
+  {/if}
+</button>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,10 @@
+<script>
+  import DarkMode from "$lib/DarkMode.svelte";
+</script>
+
 <svelte:head>
-    <title>About NullCode1337</title> 
+  <title>About NullCode1337</title>
 </svelte:head>
 
 <h1>About</h1>
+<DarkMode />
