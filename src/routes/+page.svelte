2c1d9303d1d29bfc73db39fe92a8130fd6735645
NullCode1337 <ishaqahmed.dev@gmail.com>
2024-11-19 00:17:39 +0600

    Allow abstraction of titles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
+<svelte:head>
+    <title>Home | Denza</title> 
+</svelte:head>
+
 <h1>Still WIP!</h1>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,5 @@
+<svelte:head>
+    <title>About NullCode1337</title> 
+</svelte:head>
+
 <h1>About</h1>
