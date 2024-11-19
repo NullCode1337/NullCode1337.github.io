19569bbc9233bce1eb0f23ee14bf7206d3576621
NullCode1337 <ishaqahmed.dev@gmail.com>
2024-11-19 22:46:56 +0600

    Add segoe script and fix layout

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,18 +1,21 @@
 <svelte:head>
-  <title>About NullCode1337</title>
+  <title>NullCode1337</title>
 </svelte:head>
 
-<div class="block place-content-center" id="content">
-  <video
-    autoplay
-    muted
-    loop
-    class="fixed min-w-full min-h-full right-0 bottom-0 object-cover blur-lg"
-    id="video"
-  >
-    <source src="about.mp4" type="video/mp4" />
-  </video>
-  <h1 class="fixed text-slate-50 text-xl p-20 place-content-center">
-    &lt;null&gt;
+<video
+  autoplay
+  muted
+  loop
+  class="absolute z-0 pointer-events-none min-w-full min-h-full right-0 bottom-0 object-cover blur-lg"
+>
+  <source src="about.mp4" type="video/mp4" />
+</video>
+
+<div
+  class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"
+  id="content"
+>
+  <h1 class="text-slate-50 pointer-events-none text-4xl font-segoescr">
+    &lt;Keeping myself safe&gt;
   </h1>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import typography from "@tailwindcss/typography";
+import fontFamily from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 
@@ -6,7 +7,11 @@ export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        segoescr: ["Segoe Script", ...fontFamily.fontFamily.sans],
+      },
+    },
   },
 
   plugins: [typography],
