16648c056547aa1ded7f5d6f3266e3284cd40c97
NullCode1337 <ishaqahmed.dev@gmail.com>
2024-11-20 03:35:38 +0600

    Add initial skeleton for central links widget

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -12,10 +12,21 @@
 </video>
 
 <div
-  class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"
+  class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 p-8 rounded-2xl shadow-3xl bg-black/10 backdrop-blur-xl"
   id="content"
 >
-  <h1 class="text-slate-50 pointer-events-none text-4xl font-segoescr">
+  <h1
+    class="relative text-slate-50 pointer-events-none text-4xl font-segoescr font-extrabold align-center justify-center z-20 p-0.5"
+  >
     &lt;null&gt;
   </h1>
+  <hr />
+  <br />
+  <h2>Python, Golang, Web Developer</h2>
+  <!-- <h2>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla atque
+    perferendis est molestias eius ipsa quaerat dolore doloribus, veritatis
+    ratione tempore voluptatem dolorem culpa blanditiis illo iusto voluptates,
+    quia sit?
+  </h2> -->
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ export default {
       fontFamily: {
         segoescr: ["Segoe Script", ...fontFamily.fontFamily.sans],
       },
+      dropShadow: {
+        "3xl": "0 35px 35px rgba(0, 0, 0, 0.25)",
+        "4xl": [
+          "0 35px 35px rgba(0, 0, 0, 0.25)",
+          "0 45px 65px rgba(0, 0, 0, 0.15)",
+        ],
+      },
+      boxShadow: {
+        "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+      },
     },
   },
 
