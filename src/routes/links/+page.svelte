28289a25e12c9d2f31365ebca9e247eeb07f8c49
NullCode1337 <ishaqahmed.dev@gmail.com>
2024-11-20 14:52:10 +0600

    Change button color

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -1,6 +1,5 @@
 <script>
   let value = "30";
-  let newVal = "";
   function playAudio(event) {
     if (audio.currentTime == 0) {
       audio.src = "media/flower.m4a";
@@ -16,7 +15,7 @@
     }
   }
   function changeVolume(event) {
-    newVal = String(value);
+    let newVal = String(value);
     switch (newVal.length) {
       case 2:
         newVal = Number("0." + newVal);
@@ -46,7 +45,7 @@
 </video>
 
 <div
-  class="p-4 text-center min-w-20 min-h-16 fixed mt-4 ml-4 rounded-full backdrop-blur-xl shadow-3xl text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 [&>div]:invisible [&>div]:hover:visible max-h-0 max-w-0"
+  class="p-4 text-center min-w-20 min-h-20 fixed mt-4 ml-4 rounded-full backdrop-blur-xl shadow-3xl text-white bg-slate-50/10 focus:ring-4 focus:outline-none focus:ring-slate-300 shadow-lg shadow-slate-500/50 [&>div]:invisible [&>div]:hover:visible max-h-0 max-w-0"
 >
   <button
     class="align-middle"
@@ -55,6 +54,7 @@
     id="volume"
     on:click={playAudio}
   >
+    <p class="inline float-none">Track</p>
     <i class="fa-solid fa-play inline float-none"></i>
     <i class="fa-solid fa-slash-forward inline float-none"></i>
     <i class="fa-solid fa-pause inline float-none"></i>
@@ -77,6 +77,8 @@
 <div
   class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 p-8 rounded-2xl shadow-3xl bg-black/10 backdrop-blur-xl"
   id="content"
+  on:click={playAudio}
+  onKeyDown={this.handleClick}
 >
   <h1
     class="relative text-slate-50 pointer-events-none text-4xl font-segoescr font-extrabold align-center justify-center z-20 p-0.5"
