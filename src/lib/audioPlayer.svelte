1a499a02878bb0900625f0baa17e211281274b21
NullCode1337 <ishaqahmed.dev@gmail.com>
2024-11-29 22:43:34 +0600

    Separate audioPlayer logic for better readability

---
+++ src/lib/audioPlayer.svelte
@@ -0,0 +1,44 @@
+<script context="module">
+  export function play(event, file, volume) {
+    let vol = volume || 0.3;
+
+    if (file === "") {
+      audio.volume = vol;
+    }
+    if (audio.currentTime == 0) {
+      audio.src = file;
+      audio.volume = vol;
+      audio.play();
+      return;
+    } else {
+      audio.volume = vol;
+    }
+  }
+
+  export function playAudio(event, file) {
+    play(event, file);
+  }
+
+  export function muteOrUnmute(event) {
+    audio.muted = !audio.muted;
+  }
+
+  export function changeVolume(event, value) {
+    let newVal = String(value);
+
+    switch (newVal.length) {
+      case 2:
+        newVal = Number("0." + newVal);
+        break;
+      case 1:
+        newVal = Number("0.0" + newVal);
+        break;
+      default:
+        newVal = 1.0;
+    }
+
+    play(event, "", newVal);
+  }
+</script>
+
+/* eslint-disable no-unused-vars */ /* eslint-disable no-undef */
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,17 @@
 <script>
-  let play = (volume) => {
-    let vol = volume || 0.3;
-
-    if (audio.currentTime == 0) {
-      audio.src = "media/flower.m4a";
-      audio.volume = vol;
-      audio.play();
-      return;
-    } else {
-      audio.volume = vol;
-    }
-  };
-
-  function playAudio(event) {
-    play();
-  }
-
-  function muteOrUnmute(event) {
-    audio.muted = !audio.muted;
-  }
-
   let value = "30";
-  function changeVolume(event) {
-    let newVal = String(value);
 
-    switch (newVal.length) {
-      case 2:
-        newVal = Number("0." + newVal);
-        break;
+  import {
+    playAudio,
+    muteOrUnmute,
+    changeVolume,
+  } from "$lib/audioPlayer.svelte";
 
-      case 1:
-        newVal = Number("0.0" + newVal);
-        break;
-
-      default:
-        newVal = 1.0;
-    }
-
-    play(newVal);
+  function changeVlm(event) {
+    changeVolume(event, value);
   }
+
+  let notPressed = false;
 </script>
 
 <svelte:head>
@@ -65,7 +38,7 @@
     type="button"
     aria-label="Play Music"
     id="volume"
-    on:click={playAudio}
+    on:click={(event) => playAudio(event, "media/flower.m4a")}
   >
     <p class="font-bold inline float-none text-white">Play&nbsp;</p>
     <i class="fa-solid fa-play align-middle inline float-none"></i>
@@ -82,17 +55,22 @@
     class="align-middle"
     aria-label="Mute audio"
     id="muteButton"
-    on:click={muteOrUnmute}
+    on:click={(event) => muteOrUnmute(event)}
   >
-    <i class="fa-solid fa-volume-up inline float-none"></i>
+    {#if notPressed}
+      <i class="fa-solid fa-volume-up inline float-none"></i>
+    {:else}
+      <i class="fa-solid fa-volume-mute inline float-none"></i>
+    {/if}
   </button>
+
   <input
     type="range"
     min="1"
     max="100"
-    class="accent-slate-50 align-middle ml-1"
     bind:value
-    on:input={changeVolume}
+    class="accent-slate-50 align-middle ml-1"
+    on:input={(event) => changeVlm(event)}
     id="volumeRange"
   />
 </div>
@@ -105,7 +83,8 @@
   bg-black/10"
 >
   <h1
-    class="relative text-slate-50 pointer-events-none select-none text-4xl font-segoescr font-extrabold align-center justify-center z-20 p-0.5"
+    class="relative text-slate-50 pointer-events-none select-none
+    text-4xl font-segoescr font-extrabold align-center justify-center z-20 p-0.5"
   >
     &lt;null&gt;
   </h1>
